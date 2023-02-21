7c35de6aa773e8e9169c3f54a1efdaf51430ac5f
funkypickle <46251291+funkyp1ckle@users.noreply.github.com>
2023-02-20 23:05:18 -0500

    Delete app directory

--- app/src/main/c/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := GraphicsUtils
-LOCAL_SRC_FILES := GraphicsUtils.c
-LOCAL_LDLIBS    := -lm -llog -ljnigraphics
-
-include $(BUILD_SHARED_LIBRARY)
